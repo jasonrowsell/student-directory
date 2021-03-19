66e0025ff27af66834cc727b23bbc6b066751a05
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-03-19 19:38:45 +0000

    Index printed with students. Month of cohort capitalized

--- directory.rb
+++ directory.rb
@@ -10,7 +10,6 @@ def input_students
   end
   students
 end
-	
 
 def print_header
   puts "The students of Villains Academy"
@@ -18,8 +17,8 @@ def print_header
 end
 
 def print_names(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student,index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort].capitalize} cohort)"
   end
 end
 
